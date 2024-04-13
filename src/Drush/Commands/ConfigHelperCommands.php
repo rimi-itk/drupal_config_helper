b0b525e5274c1e53ee9f35ea0b177fccb1765681
Mikkel Ricky <rimi@aarhus.dk>
2024-04-13 07:01:31 +0200

    Cleaned up

--- src/Drush/Commands/ConfigHelperCommands.php
+++ src/Drush/Commands/ConfigHelperCommands.php
@@ -7,14 +7,16 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Site\Settings;
 use Drush\Attributes as CLI;
+use Drush\Commands\AutowireTrait;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\Console\Exception\RuntimeException;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * A Drush commandfile.
  */
 final class ConfigHelperCommands extends DrushCommands {
+
+  use AutowireTrait;
 
   /**
    * Constructs a ConfigHelperCommands object.
@@ -28,17 +30,6 @@ final class ConfigHelperCommands extends DrushCommands {
   }
 
   /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container): static {
-    return new static(
-      $container->get('module_handler'),
-      $container->get('config.factory'),
-      $container->get('file_system'),
-    );
-  }
-
-  /**
    * Rename config.
    */
   #[CLI\Command(name: 'config_helper:rename')]
@@ -48,16 +39,21 @@ final class ConfigHelperCommands extends DrushCommands {
   #[CLI\Option(name: 'dry-run', description: 'Dry run.')]
   #[CLI\Usage(name: 'drush config:rename porject project', description: 'Fix typo in config.')]
   #[CLI\Usage(name: "drush config:rename 'field_(.+)' '\1' --regex", description: 'Remove superfluous prefix from field machine names.')]
-  public function rename(string $search, string $replace, array $options = [
-    'regex' => FALSE,
-    'dry-run' => FALSE,
-  ]): void {
+  public function rename(
+    string $search,
+    string $replace,
+    array $options = [
+      'regex' => FALSE,
+      'dry-run' => FALSE,
+    ]
+  ): void {
     if (!$options['regex']) {
       $search = '/' . preg_quote($search, '/') . '/';
     }
 
-    $names = $this->configFactory->listAll();
+    $this->io()->writeln(sprintf('Replacing %s with %s in config', $search, $replace));
 
+    $names = $this->configFactory->listAll();
     foreach ($names as $name) {
       $config = $this->configFactory->getEditable($name);
       $data = $this->replaceKeysAndValues($search, $replace, $config->get());
@@ -66,7 +62,7 @@ final class ConfigHelperCommands extends DrushCommands {
 
       if (preg_match($search, $name)) {
         $newName = preg_replace($search, $replace, $name);
-        $this->output()->writeln(sprintf('%s -> %s', $name, $newName));
+        $this->io()->writeln(sprintf('%s -> %s', $name, $newName));
         if (!$options['dry-run']) {
           $this->configFactory->rename($name, $newName);
         }
@@ -80,46 +76,39 @@ final class ConfigHelperCommands extends DrushCommands {
    * @see https://www.drupal.org/node/2087879#s-example:~:text=The%20dependencies%20and%20enforced%20keys%20ensure,removed%20when%20the%20module%20is%20uninstalled
    */
   #[CLI\Command(name: 'config_helper:enforce-module-dependency')]
-  #[CLI\Argument(name: 'modules', description: 'The module names.')]
+  #[CLI\Argument(name: 'module', description: 'The module name.')]
+  #[CLI\Argument(name: 'configNames', description: 'The config names.')]
   #[CLI\Option(name: 'remove-uuid', description: 'Remove uuid and _core from config.')]
+  #[CLI\Option(name: 'dry-run', description: 'Dry run.')]
   #[CLI\Usage(name: 'drush config:enforce-module-dependency my_module another_module', description: '')]
-  public function enforceModuleDependencies(array $modules, array $options = [
-    'remove-uuid' => FALSE,
-  ]): void {
-    foreach ($modules as $module) {
-      if (!$this->moduleHandler->moduleExists($module)) {
-        throw new RuntimeException(sprintf('Invalid module: %s', $module));
+  public function enforceModuleDependencies(
+    string $module,
+    array $configNames,
+    array $options = [
+      'remove-uuid' => FALSE,
+      'dry-run' => FALSE,
+    ]
+  ): void {
+    if (!$this->moduleHandler->moduleExists($module)) {
+      throw new RuntimeException(sprintf('Invalid module: %s', $module));
+    }
+
+    if (empty($configNames)) {
+      $configNames = $this->getModuleConfigNames($module);
+    }
+
+    foreach ($configNames as $name) {
+      $this->io()->writeln(sprintf('Config name: %s', $name));
+      if (!$this->configExists($name)) {
+        throw new RuntimeException(sprintf('Invalid config name: %s', $name));
       }
 
-      $this->output()->writeln($module);
-
-      $names = array_values(array_filter($this->configFactory->listAll(), static function ($name) use ($module) {
-        return preg_match('/[._]' . preg_quote($module, '/') . '/', $name);
-      }));
-
-      foreach ($names as $name) {
-        $this->output()->writeln($name);
+      if (!$options['dry-run']) {
         $config = $this->configFactory->getEditable($name);
 
         // Config::merge does merge correctly so we do it ourselves.
         $dependencies = $config->get('dependencies') ?? [];
-
-        if (!isset($dependencies['module'])) {
-          $dependencies['module'] = [];
-        }
-        if (!isset($dependencies['enforced'])) {
-          $dependencies['enforced'] = [];
-        }
-        if (!isset($dependencies['enforced']['module'])) {
-          $dependencies['enforced']['module'] = [];
-        }
-
-        $dependencies['module'][] = $module;
         $dependencies['enforced']['module'][] = $module;
-
-        $dependencies['module'] = array_unique($dependencies['module']);
-        sort($dependencies['module']);
-
         $dependencies['enforced']['module'] = array_unique($dependencies['enforced']['module']);
         sort($dependencies['enforced']['module']);
 
@@ -139,53 +128,69 @@ final class ConfigHelperCommands extends DrushCommands {
    * Move config info config/install folder in a module.
    */
   #[CLI\Command(name: 'config_helper:move-module-config')]
-  #[CLI\Argument(name: 'modules', description: 'The module names.')]
+  #[CLI\Argument(name: 'module', description: 'The module name.')]
+  #[CLI\Argument(name: 'configNames', description: 'The config names.')]
   #[CLI\Option(name: 'source', description: 'Config source directory.')]
-  #[CLI\Usage(name: 'drush config:move-module-config my_module another_module', description: '')]
-  #[CLI\Usage(name: 'drush config:move-module-config --source=sites/all/config my_module another_module', description: '')]
-  public function moveModuleConfig(array $modules, array $options = [
-    'source' => NULL,
-  ]): void {
-    foreach ($modules as $module) {
-      if (!$this->moduleHandler->moduleExists($module)) {
-        throw new RuntimeException(sprintf('Invalid module: %s', $module));
-      }
+  #[CLI\Option(name: 'enforced', description: 'Move only config with enforced dependency on the module.')]
+  #[CLI\Option(name: 'dry-run', description: 'Dry run.')]
+  #[CLI\Usage(name: 'drush config:move-module-config my_module', description: '')]
+  #[CLI\Usage(name: 'drush config:move-module-config --source=sites/all/config my_module', description: '')]
+  public function moveModuleConfig(
+    string $module,
+    array $configNames,
+    array $options = [
+      'source' => NULL,
+      'enforced' => FALSE,
+      'dry-run' => FALSE,
+    ]
+  ): void {
+    if (!$this->moduleHandler->moduleExists($module)) {
+      throw new RuntimeException(sprintf('Invalid module: %s', $module));
+    }
 
-      $this->output()->writeln($module);
-
-      $names = array_values(array_filter($this->configFactory->listAll(), static function ($name) use ($module) {
-        return preg_match('/[._]' . preg_quote($module, '/') . '/', $name);
-      }));
-
-      $source = $options['source'] ?? Settings::get('config_sync_directory');
-      if (NULL === $source) {
-        throw new RuntimeException('Config source not defined');
+    if ($options['enforced']) {
+      $configNames = $this->getEnforcedModuleConfigNames($module);
+    } else {
+      if (empty($configNames)) {
+        $configNames = $this->getModuleConfigNames($module);
       }
-      $configPath = DRUPAL_ROOT . '/' . $source;
-      if (!is_dir($configPath)) {
-        throw new RuntimeException(sprintf('Config directory %s does not exist', $configPath));
-      }
-      $modulePath = $this->moduleHandler->getModule($module)->getPath();
-      $moduleConfigPath = $modulePath . '/config/install';
+    }
+
+    $source = $options['source'] ?? Settings::get('config_sync_directory');
+    if (NULL === $source) {
+      throw new RuntimeException('Config source not defined');
+    }
+    $configPath = DRUPAL_ROOT . '/' . $source;
+    if (!is_dir($configPath)) {
+      throw new RuntimeException(sprintf('Config directory %s does not exist',
+        $configPath));
+    }
+    $modulePath = $this->moduleHandler->getModule($module)->getPath();
+    $moduleConfigPath = $modulePath . '/config/install';
+    if (!$options['dry-run']) {
       if (!is_dir($moduleConfigPath)) {
         $this->fileSystem->mkdir($moduleConfigPath, 0755, TRUE);
       }
+    }
 
-      foreach ($names as $name) {
-        $this->output()->writeln($name);
+    foreach ($configNames as $name) {
+      $this->io()->writeln($name);
 
-        $filename = $name . '.yml';
-        $source = $configPath . '/' . $filename;
-        $destination = $moduleConfigPath . '/' . $filename;
+      $filename = $name . '.yml';
+      $source = $configPath . '/' . $filename;
+      $destination = $moduleConfigPath . '/' . $filename;
 
-        if (!file_exists($source)) {
-          $this->output()->writeln(sprintf('Source file %s does not exist', $source));
-          continue;
-        }
-
-        $this->fileSystem->move($source, $destination, FileSystemInterface::EXISTS_REPLACE);
-        $this->output()->writeln(sprintf('%s -> %s', $source, $destination));
+      if (!file_exists($source)) {
+        $this->output()->writeln(sprintf('Source file %s does not exist',
+          $source));
+        continue;
       }
+
+      if (!$options['dry-run']) {
+        $this->fileSystem->move($source, $destination,
+          FileSystemInterface::EXISTS_REPLACE);
+      }
+      $this->io()->writeln(sprintf('%s -> %s', $source, $destination));
     }
   }
 
@@ -194,7 +199,11 @@ final class ConfigHelperCommands extends DrushCommands {
    *
    * @see https://stackoverflow.com/a/29619470
    */
-  private function replaceKeysAndValues(string $search, string $replace, array $input): array {
+  private function replaceKeysAndValues(
+    string $search,
+    string $replace,
+    array $input
+  ): array {
     $return = [];
     foreach ($input as $key => $value) {
       if (preg_match($search, $key)) {
@@ -212,6 +221,33 @@ final class ConfigHelperCommands extends DrushCommands {
     }
 
     return $return;
+  }
+
+  private function configExists(string $name): bool {
+    return in_array($name, $this->configFactory->listAll(), TRUE);
+  }
+
+  private function getModuleConfigNames(string $module): array {
+  // Find config names containing the module name.
+  return array_values(array_filter($this->configFactory->listAll(),
+    static function($name) use ($module) {
+      return preg_match('/[.]' . preg_quote($module, '/') . '(?:$|[.])/',
+        $name);
+    }));
+  }
+
+  private function getEnforcedModuleConfigNames(string $module): array {
+    $configNames = [];
+    $names = $this->configFactory->listAll();
+    foreach ($names as $name) {
+      $config = $this->configFactory->get($name)->getRawData();
+      if (isset($config['dependencies']['enforced']['module'])
+        && in_array($module, $config['dependencies']['enforced']['module'], TRUE)) {
+        $configNames[] = $name;
+      }
+    }
+
+    return $configNames;
   }
 
 }
